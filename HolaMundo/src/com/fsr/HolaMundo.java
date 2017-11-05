0c914c5434eb5e8af585691eb04d021ac9ee30ad
usuario <usuario@profesor>
2017-11-05 16:02:59 +0100

    Revert "Añadido comentario al código"
    This reverts commit d5959e5df6138006a9ebae0e303757ac045d67f9.

--- HolaMundo/src/com/fsr/HolaMundo.java
+++ HolaMundo/src/com/fsr/HolaMundo.java
@@ -3,7 +3,7 @@ package com.fsr;
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		//Cambio realizado por FSR
+		
 		System.out.println("Hola mundo");
 		System.out.println("Hello world");
 
